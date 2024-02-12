610bf8e33cb71cf3e1a720681d3f769e1cb41434
rohit0814 <singhrohit.rs747@gmail.com>
2024-02-12 15:21:41 +0530

    complete this project

--- React-todoList/src/App.jsx
+++ React-todoList/src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import TodoHeader from "./Components/TodoHeader";
+import LayoutComponent from "./Components/LayoutComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputTodoList from "./Components/InputTodoList";
+import TodoList from "./Components/TodoLists";
+import ErrorMessage from "./Components/ErrorMessage";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todo,setTodo] = useState([]);
+
+  const inputTodo = (title,timing) =>{
+    if(title!=="" && timing!==""){
+    const newTodo = [...todo,{id: todo.length+1 ,title: title,timing: timing}]
+    setTodo(newTodo);
+    }
+  }
+
+  const deleteList = (key) => {
+    const newList = todo.filter((item)=>item.id!=key)
+    setTodo(newList);
+  }
 
   return (
     <>
-      <h1>Todo</h1>
+      <center>
+        <LayoutComponent>
+          <TodoHeader />
+          <InputTodoList inputTodo={inputTodo}/>
+          <ErrorMessage todo={todo}/>
+          <TodoList todo={todo} deleteList={deleteList}/>
+        </LayoutComponent>
+      </center>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
